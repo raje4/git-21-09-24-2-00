c6373ebd4dcb1c0b71795f47755ec7125389a8ed
koti <krao.netapp@gmail.com>
2024-09-21 14:59:12 +0530

    hii

---
+++ src/main/java/com/neoteric/ticketdemo/App.java
@@ -0,0 +1,13 @@
+package com.neoteric.ticketdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
